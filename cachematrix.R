6c9d3e52a0eaa4e9f90268f4eb2d408c89bd4ff3
tterpstra <tdterpstra@earthlink.net>
2015-03-21 13:45:38 -0500

    Programming Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This creates functions to write a matrix and its inverse to a cache
+## and then read the matrix and inverse from the cache. It also provides
+## functionality to subsequently use the matrix and inverse from the cache
 
-## Write a short comment describing this function
-
+## Create functions to Write matrix and inverse inverse into cache and read from cache
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    setmatrix <- function(y = NULL){
+        m <<- y
+    }
+    setinverse <- function(y = NULL) {
+        mi <<- solve(y)
+    }
+    getmatrix <- function() m
+    getinverse <- function() mi
+    list(setmatrix = setmatrix, setinverse = setinverse, getmatrix = getmatrix, getinverse = getinverse)
+     
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Read inverse from cache if it exists, otherwise calc inverse
+cacheSolve <- function(x, m, ...) {
+        oldm <- x$getmatrix()
+## Need to see if matrix has been cached, and is same matrix passed to this function        
+        if (!is.null(oldm) && identical(m, oldm)) {
+            return(x$getinverse())
+        } else return(solve(m))
 }
